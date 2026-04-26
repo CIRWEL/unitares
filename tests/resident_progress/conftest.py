3e7253a951f81d666f981de2f7679d5cdc3c166a
Kenny Wang <cirwel@The-CIRWEL-Group.local>
2026-04-25 20:25:40 -0600

    test(resident-progress): add loop_scope=session to async fixture

--- tests/resident_progress/conftest.py
+++ tests/resident_progress/conftest.py
@@ -6,6 +6,6 @@ import pytest_asyncio
 from tests.test_db_utils import ensure_test_database_schema
 
 
-@pytest_asyncio.fixture(scope="session", autouse=True)
+@pytest_asyncio.fixture(scope="session", loop_scope="session", autouse=True)
 async def _bootstrap_schema():
     await ensure_test_database_schema()
